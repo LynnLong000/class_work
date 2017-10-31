837c1d822f7ed0617a245d51cce4dc7f5aabb2fa
Lynn Long Jr <lynnlong000@gmail.com>
2017-10-31 00:33:23 +0000

    2 out of 3

---
+++ classathon1/factorialize_a_number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void){
+
+    int num = get_int("number you want factorialized: ");
+    long long count = 1;
+
+    for(int i = 1; i <= num; i++){
+        count *= i;
+    }
+
+    printf("factorialized:%lli \n",count);
+}
---
+++ classathon1/sum_all_odd_fibonacci_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void){
+
+    int num = get_int("what would you like to see sum of all off fibonacci: ");
+    int prevNumber = 0;
+    int currNumber = 1;
+    int result = 0;
+
+    while (currNumber <= num){
+        if((currNumber%2) != 0){
+            result = result + currNumber;
+        }
+
+        currNumber = currNumber + prevNumber;
+        prevNumber = currNumber - prevNumber;
+    }
+
+    printf("Sum of all odd fibonacci numbers: %i \n",result);
+}
