6ba6436f2cd206ca6ff40332230f204d727c9a4b
Lynn Long Jr <lynnlong000@gmail.com>
2017-11-01 03:36:11 +0000

    added is prime funcition

--- classathon1/sum_all_primes.c
+++ classathon1/sum_all_primes.c
@@ -5,3 +5,14 @@ int main (void){
 
 
 }
+
+
+
+int isPrime(int num){
+    for(int i = 2; i <= num; i++){
+        if((num % i) == 0 && number != i){
+            return false;
+        }
+    }
+    return true;
+}
