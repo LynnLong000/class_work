d3004e9265a56cd9ba34f879e7aff6a18082a7fd
Lynn Long Jr <lynnlong000@gmail.com>
2017-11-01 04:52:52 +0000

    finished the assignment maybe lol

--- classathon1/sum_all_primes.c
+++ classathon1/sum_all_primes.c
@@ -2,8 +2,14 @@
 #include <cs50.h>
 
 int main (void){
-
-
+    int num = get_int("Sum of all primes ends: ");
+    int sum = 0;
+    for(int i = 2; i <= num; i++){
+        if(isPrime(i) == true){
+            sum = sum + i;
+        }
+    }
+    printf("sum of all primes up to %i is %i",num,sum);
 }
 
 
