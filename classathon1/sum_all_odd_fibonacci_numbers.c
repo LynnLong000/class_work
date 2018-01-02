53854f9c1bb89123f8a97c5f475e85437622efac
Lynn Long Jr <lynnlong000@gmail.com>
2018-01-02 22:21:31 +0000

    adding some changes

--- classathon1/sum_all_odd_fibonacci_numbers.c
+++ classathon1/sum_all_odd_fibonacci_numbers.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (void){
-
+int main (void)
+{
     int num = get_int("what would you like to see sum of all off fibonacci: ");
     int prevNumber = 0;
     int currNumber = 1;
     int result = 0;
 
-    while (currNumber <= num){
-        if((currNumber%2) != 0){
+    while (currNumber <= num)
+    {
+        if((currNumber%2) != 0)
+        {
             result = result + currNumber;
         }
 
